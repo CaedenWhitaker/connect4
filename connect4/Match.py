40661c5c2c1f775b55767a20ac7b031226755712
CaedenWhitaker <caeden.whitaker@gmail.com>
2022-07-19 23:40:34 -0400

    Remove DB from this class

--- connect4/Match.py
+++ connect4/Match.py
@@ -1,15 +1,9 @@
-from multiprocessing import connection
-
-from matplotlib.pyplot import connect
 from connect4.MouseListener import MouseListener
 from connect4.VisualElement import VisualElement
 from connect4.Player import Player
 from connect4.Board import Board
-from connect4.Constants import DATABASE_PATH
 import math
 import pygame
-import sqlite3
-import os
 
 
 class Match(VisualElement):
@@ -73,33 +67,6 @@ class Match(VisualElement):
 				MouseListener.clear()
 			self.player1.setTurn(self.turn)
 			self.player2.setTurn(self.turn)
-			
-
-	def save(self):
-		# fileFound = os.path.isfile(DATABASE_PATH)
-		# if not fileFound and os.path.exists(DATABASE_PATH):
-		# 	return False
-		# try:
-		# 	connection = sqlite3.connect(DATABASE_PATH)
-		# except sqlite3.DatabaseError:
-		# 	pass
-		connection = sqlite3.connect(DATABASE_PATH)
-		cursor = connection.cursor()
-		cursor.execute("""
-		CREATE TABLE Matches(
-			id INTEGER PRIMARY KEY NOT NULL,
-			start TIME NOT NULL,
-			end TIME NOT NULL,
-			player1 char[3] NOT NULL,
-			player2 char[3] NOT NULL,
-			
-		)
-		""")
-
-		
-
-
-
 
 	def render(self):
 		self.renderBackground()
